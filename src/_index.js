8ceaa65d319cb55cc61508fcf5163afc54526e2d
axdg <axdg@dfant.asia>
2017-04-08 10:52:27 +1000

    send implementation

--- src/_index.js
+++ src/_index.js
@@ -1,1 +1,59 @@
-_index.js
+/**
+ * The channel constructor function, takes a
+ * single argument - the capacity.
+ *
+ * @param {Number}
+ *
+ * @returns {Object}
+ */
+function Channel(capacity = 1) {
+  const queued = [];
+  const inbound = [];
+  const outbound = [];
+
+  let closed = false;
+
+  // Type check the arguments.
+  if (typeof capacity !== 'number' || capacity < 1) {
+    throw new Error('capacity must be a non-zero positive integer');
+  }
+
+  this.queue = function (value) {
+    if (value !== undefined) {
+      return new Promise(function (resolve, reject) {
+        // The channel is closed... sending is not possible.
+        if (closed === true) {
+          return reject(new Error('attempt to send to a closed channel'));
+        }
+
+        // The channel should be closed.
+        if (value === null) {
+          closed = true;
+          return resolve();
+        }
+
+        // The internal buffer is at capacity.
+        if (queued.length === capacity) {
+          return inbound.push(function () {
+            queued.push(value);
+            if (outbound.length > 0) {
+              outbound.shift()();
+            }
+
+            return resolve(value);
+          });
+        }
+
+        return queued.push(function () {
+          if (outbound.length > 0) {
+            outbound.shift()();
+          }
+
+          return resolve(value);
+        });
+      });
+    }
+
+    return
+  };
+}
